3b46b7e01156a00a0555949343f5cc13a4ad4634
Nina Frings <nina.frings@unibas.ch>
2023-09-29 11:09:35 +0200

    changes

---
+++ publishing.R
@@ -0,0 +1,11 @@
+### how to publish
+
+publish_site(
+  site_dir = ".",
+  site_name = NULL,
+  method = c("rsconnect"),
+  server = NULL,
+  account = NULL,
+  render = TRUE,
+  launch_browser = interactive()
+)
